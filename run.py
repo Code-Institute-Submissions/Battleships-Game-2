32369c599363998130a9d8f9fb8f063d1d4a68ca
ericmcg96 <ericmcg4@gmail.com>
2021-11-24 15:09:22 +0000

    create the gameboard

--- run.py
+++ run.py
@@ -5,3 +5,13 @@ MAX_GUESSES = 20
 SHIPS = [random.randint(0, 49) for i in range(5)]
 CORRECT = 0
 PLACE = 0
+
+print('Choose a target between 0 and 49.\n''Hit 3 Battleships to win.\n')
+print("       Let's play Battleships       ")
+print('     0  1  2  3  4  5  6  7  8  9      ')
+for row in range(5):
+    location = ' '
+    for column in range(10):
+        character = ' - '
+        location = location + character
+    print(row, location)
