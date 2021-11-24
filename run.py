dcd06f352174a5075dd31fc428e2c9a93a91a647
ericmcg96 <ericmcg4@gmail.com>
2021-11-24 15:43:21 +0000

    Give feedback to the user

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ num_of_guesses = 0
 max_guesses = 20
 ships = [random.randint(0, 49) for i in range(5)]
 correct = 0
-Pplace = 0
+place = 0
 
 print('Choose a target between 0 and 49.\n''Hit 3 Battleships to win.\n')
 print("       Let's play Battleships       ")
@@ -18,7 +18,7 @@ for row in range(5):
 
 used_guesses = []
 while num_of_guesses < max_guesses:
-    guess = int(input('Choose your target: '))
+    guess = int(input('Choose your target:\n'))
     num_of_guesses += 1
     if guess in used_guesses and guess in ships:
         correct -= 1
@@ -28,22 +28,37 @@ while num_of_guesses < max_guesses:
         used_guesses.remove(guess)
     used_guesses.append(guess)
 
-place = 0
-print('             Battleships           ')
-print('    0  1  2  3  4  5  6  7  8  9      ')
-for row in range(5):
-    location = ' '
-    for character in range(10):
-        character = ' - '
-        if guess == place and guess in ships:
-            character = ' X '
-            correct += 1
-        if guess == place and guess not in ships:
-            character = ' O '
-        if place in used_guesses and place in ships:
-            character = ' X '
-        if place in used_guesses and place not in ships:
-            character = ' O '
+    place = 0
+    print('             Battleships           ')
+    print('    0  1  2  3  4  5  6  7  8  9      ')
+    for row in range(5):
+        location = ' '
+        for character in range(10):
+            character = ' - '
+            if guess == place and guess in ships:
+                character = ' X '
+                correct += 1
+            if guess == place and guess not in ships:
+                character = ' O '
+            if place in used_guesses and place in ships:
+                character = ' X '
+            if place in used_guesses and place not in ships:
+                character = ' O '
             place += 1
             location = location + character
         print(row, location)
+
+    if guess in ships:
+        print('              hit!        ')
+    else:
+        print('              Miss!        ')
+
+    print('No. of guesses used: ', num_of_guesses)
+    print('hits: ', correct)
+    print('Total Number of guesses', max_guesses)
+
+
+
+
+
+
