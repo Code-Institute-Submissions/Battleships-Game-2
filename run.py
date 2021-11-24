676f350b1e6cdb1b8542c440700423ddbe1364f8
ericmcg96 <ericmcg4@gmail.com>
2021-11-24 15:20:31 +0000

    create a while loop that takes input and handles already used guesses

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 import random
 
-NUM_OF_GUESSES = 0
-MAX_GUESSES = 20
-SHIPS = [random.randint(0, 49) for i in range(5)]
-CORRECT = 0
-PLACE = 0
+num_of_guesses = 0
+max_guesses = 20
+ships = [random.randint(0, 49) for i in range(5)]
+correct = 0
+Pplace = 0
 
 print('Choose a target between 0 and 49.\n''Hit 3 Battleships to win.\n')
 print("       Let's play Battleships       ")
@@ -15,3 +15,15 @@ for row in range(5):
         character = ' - '
         location = location + character
     print(row, location)
+
+used_guesses = []
+while num_of_guesses < max_guesses:
+    guess = int(input('Choose your target: '))
+    num_of_guesses += 1
+    if guess in used_guesses and guess in ships:
+        correct -= 1
+    if guess in used_guesses:
+        print('           ALREADY GUESSED!          ')
+        num_of_guesses -= 1
+        used_guesses.remove(guess)
+    used_guesses.append(guess)
