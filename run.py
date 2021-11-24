0e5659114eaa34960b126216a26899736d7ae822
ericmcg96 <ericmcg4@gmail.com>
2021-11-24 16:14:46 +0000

    Add comments

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
+# Imports the random module
 import random
 
 
+# This function contains the gameboard
 def game_board():
     num_of_guesses = 0
     max_guesses = 20
@@ -8,6 +10,7 @@ def game_board():
     correct = 0
     place = 0
 
+    # Here is the start of the printed gameboard
     print('Choose a target between 0 and 49.\n''Hit 3 Battleships to win.\n')
     print("       Let's play Battleships       ")
     print('     0  1  2  3  4  5  6  7  8  9      ')
@@ -18,20 +21,29 @@ def game_board():
             location = location + character
         print(row, location)
 
+    # Targets already guessed will be appended to this list
     used_guesses = []
+
+    # While loop that contains the gameboard after each guess
     while num_of_guesses < max_guesses:
+        # Try statement
         try:
+            # Users input
             guess = int(input('Choose your target:\n'))
             num_of_guesses += 1
+            # Handles already correct guesses
             if guess in used_guesses and guess in ships:
                 correct -= 1
+            # Handles already made guesses
             if guess in used_guesses:
                 print('           ALREADY GUESSED!          ')
                 num_of_guesses -= 1
                 used_guesses.remove(guess)
+            # Appends guesses already made to the used_guesses list
             used_guesses.append(guess)
 
             place = 0
+            # Updated gameboard
             print('             Battleships           ')
             print('    0  1  2  3  4  5  6  7  8  9      ')
             for row in range(5):
@@ -51,28 +63,36 @@ def game_board():
                     location = location + character
                 print(row, location)
 
+            # Hit message
             if guess in ships:
                 print('              hit!        ')
+
+            # Miss message
             else:
                 print('              Miss!        ')
 
+            # Game info
             print('No. of guesses used: ', num_of_guesses)
             print('hits: ', correct)
             print('Total Number of guesses', max_guesses)
 
+            # Out of bounds message
             if guess < 0 or guess > 49:
                 print('Thats out of bounds, try again.')
                 used_guesses.remove(guess)
                 num_of_guesses -= 1
 
+            # Losing message
             if num_of_guesses == max_guesses:
                 print('You lose, max number of guesses used.')
                 break
 
+            # Winning message
             if correct == 3:
                 print('You Win!')
                 break
 
+        # Exception message
         except ValueError:
             print('Invalid input, try again')
 
